203dba1d0d105ad2282a6568d7df7e2868c8870a
Matthew Evans <matt.evans@centeva.com>
2025-07-01 19:08:42 -0600

    Removed unnecessary additional validation for Circle constructor.

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -2,7 +2,7 @@ package shapes
 
 class Circle(center: Point, radiusX: Double, radiusY:Double) : Ellipse(center, radiusX, radiusY) {
     init {
-        require(radiusX == radiusY && radiusX > 0.0 && radiusX.isFinite()) {
+        require(radiusX == radiusY) {
             "Both radii must be equal in a circle."
         }
     }
